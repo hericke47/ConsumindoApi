1b3b4e8a9a2fa189feac2142a28cd614f4ef86b9
Herick Exterkoetter <herickherick47@gmail.com>
2020-08-04 00:21:04 -0300

    api pelo backend consumida

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,21 @@
 const cors = require('cors');
 const express = require('express');
 const app = express();
+const axios = require ('axios');
 
 app.use(cors());
 
-app.get('/', (request, response) => {
-    return response.json([
-        {name: 'herick'},
-        {name: 'Diego'}
-    ])
+app.get('/', async(request, response) => {
+
+    try {
+    // response é a responsta do axios mas eu tiro o data de dentro do response
+        const { data } = await axios('https://jsonplaceholder.typicode.com/users')
+        return response.json(data)
+    } catch (error) {
+        console.log(error);        
+    }
+
+
 });
 
 app.listen('3333');
